5da6fdd02c7e7dbfb57a87e584b8230a31b9db7e
jmjulioenrique <julio.enrique@dive.tv>
2017-11-20 22:22:47 +0100

    Npm minor with skip

--- scr/publish.sh
+++ scr/publish.sh
@@ -5,7 +5,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" == "master" ]; then
         echo "Set minor version and push on master branch, publishing lib to NPM"
 	npm build
-	npm version minor
+	npm version minor -m "[skip ci]"
         npm publish
     else
         echo "Branch is not master, skipping NPM publish"

23cc129ddc91458942c5d36cc74d4be58b104b98
Julio Enrique <jmjulioenrique@gmail.com>
2017-11-20 17:46:19 +0100

    Added travis-ci

---
+++ scr/publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+      
+    if [ "$TRAVIS_BRANCH" == "master" ]; then
+        echo "Push on master branch, publishing lib to NPM"
+	npm minor
+        npm publish
+    else
+        echo "Branch is not master, skipping NPM publish"
+    fi
+else
+    echo "Pull request, skipping NPM publish"
+fi

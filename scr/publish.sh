bb489049ec9a026c29d9dd6e646a8368eeedbcfe
Julio Enrique <jmjulioenrique@gmail.com>
2017-11-20 18:12:36 +0100

    Modified publish

--- scr/publish.sh
+++ scr/publish.sh
@@ -3,7 +3,7 @@
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
       
     if [ "$TRAVIS_BRANCH" == "master" ]; then
-        echo "Push on master branch, publishing lib to NPM"
+        echo "Set minor version and push on master branch, publishing lib to NPM"
 	npm version minor
         npm publish
     else

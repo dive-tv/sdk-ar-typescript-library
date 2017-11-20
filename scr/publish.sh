bd6cb4c24f072ce0f6e4f752b17b8ae0875c3db6
Julio Enrique <jmjulioenrique@gmail.com>
2017-11-20 18:08:05 +0100

    Modified minor nom version

--- scr/publish.sh
+++ scr/publish.sh
@@ -4,7 +4,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
       
     if [ "$TRAVIS_BRANCH" == "master" ]; then
         echo "Push on master branch, publishing lib to NPM"
-	npm minor
+	npm version minor
         npm publish
     else
         echo "Branch is not master, skipping NPM publish"

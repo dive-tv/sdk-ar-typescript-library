ab438a207e3b01712b143b53eecd3c0c298f46b7
Julio Enrique <jmjulioenrique@gmail.com>
2017-11-21 15:50:20 +0100

    Modif. Run build

--- scr/publish.sh
+++ scr/publish.sh
@@ -5,7 +5,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" == "master" ]; then
         echo "Set minor version and push on master branch, publishing lib to NPM"
 	npm install
-	npm build
+	npm run build
 	ls -la
         npm publish
     else

fe2e8dbc1a0fcd34948b97f0896410a56cbd6290
Julio Enrique <jmjulioenrique@gmail.com>
2017-11-21 12:50:51 +0100

    Deleted auto version minor

--- scr/publish.sh
+++ scr/publish.sh
@@ -4,10 +4,9 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
       
     if [ "$TRAVIS_BRANCH" == "master" ]; then
         echo "Set minor version and push on master branch, publishing lib to NPM"
-	#npm build
-	npm install
-	npm version minor --no-git-tag-version -m "[skip ci]"
-    npm publish
+	    npm build
+	    npm install
+        npm publish
 
     else
         echo "Branch is not master, skipping NPM publish"

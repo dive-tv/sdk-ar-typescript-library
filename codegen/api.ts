46c3daf09863b5ec0270a67b8b399501edaffb2d
Travis CI User <travis@example.org>
2018-04-25 13:12:51 +0000

    Auto generated

--- codegen/api.ts
+++ codegen/api.ts
@@ -1506,6 +1506,7 @@ export class CustomAPI extends DefaultApi {
     const expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
   }
+  
   public postTokenAndSave(params?: {grantType: string, username?: string, password?: string, deviceId?: string, refreshToken?: string, clientId?: string}, options?: any) {
     this.deleteSavedToken();
     return new Promise((resolve: any, reject: any) => {
